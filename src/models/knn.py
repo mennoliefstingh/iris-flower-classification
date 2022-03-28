Further improvement of method, fixed data leakage issue with cross validation (by moving away from cross validation), addition of documentation for some more complicated functions

src/models/knn.py
@@ -4,13 +4,24 @@ import os.path
 import json
 import numpy as np
 
-def _optimal_parameters_knn(filepath:str, X:np.ndarray, y:np.ndarray) -> dict:
-    print("looking for optimal params")
-    if os.path.exists(filepath):
+def _optimal_parameters_knn(filepath:str, X:np.ndarray, y:np.ndarray, use_cached:bool=True) -> dict:
+    """
+    Finds optimal parameters for the KNN classifier using a (pre-defined) grid search. 
+
+    Keyword arguments:
+        filepath: The file location for cached parameters
+        X: Training data
+        y: target labels
+        use_cached: True by default, set to fals if param search has to be redone
+
+    Returns: 
+        A dictionary containing optimal parameters for this training set. Also saves to file. 
+
+    """
+    if os.path.exists(filepath) and use_cached:
         with open(filepath, 'r') as file:
             return json.load(file)
     else:
-        print("no param file found")
         param_grid = {'n_neighbors': np.arange(1, 10, 1).tolist()}
         best_params = find_optimal_parameters(KNeighborsClassifier(), param_grid, X, y)
         with open('src/models/knn_params.json', 'w') as file:
@@ -18,10 +29,35 @@ def _optimal_parameters_knn(filepath:str, X:np.ndarray, y:np.ndarray) -> dict:
         return best_params
 
 def get_untrained_knn_classifier(X, y):
+    """
+    Returns an untrained KNN classifier with optimal parameters found. If there are no
+    cached optimal parameters, these will be found using the supplied X and y. 
+    IMPORTANT: To prevent leakage, do *not* pass the entire dataset as that will influence parameter selection
+
+    Keyword arguments:
+        X: Training data
+        y: target labels
+        use_cached: True by default, set to false if param search has to be redone
+
+    Returns: 
+        An untrained KNN classifier with optimal parameters based on X/y sets. 
+    """
     optimal_params = _optimal_parameters_knn('src/models/knn_params.json', X, y)
     return KNeighborsClassifier(**optimal_params)
 
 def get_trained_knn_classifier(X, y):
+    """"
+    Returns a trained KNN classifier with optimal parameters found. If there are no
+    cached optimal parameters, these will be found using the supplied X and y. 
+
+    Keyword arguments:
+    X: data
+    y: target labels
+    use_cached: True by default, set to false if param search has to be redone
+
+    Returns: 
+    A KNN classifier trained on 
+    """
     optimal_params = _optimal_parameters_knn('src/models/knn_params.json', X, y)
     clf = KNeighborsClassifier(**optimal_params)
     return clf.fit(X, y)

src/models/random_forest.py
@@ -4,8 +4,21 @@ import os.path
 import json
 import numpy as np
 
-def _optimal_parameters_rf(filepath:str, X:np.ndarray, y:np.ndarray) -> dict:
-    if os.path.exists(filepath):
+def _optimal_parameters_rf(filepath:str, X:np.ndarray, y:np.ndarray, use_cached:bool=True) -> dict:
+    """
+    Finds optimal parameters for the random forest classifier using a (pre-defined) grid search. 
+
+    Keyword arguments:
+        filepath: The file location for cached parameters
+        X: Training data
+        y: target labels
+        use_cached: True by default, set to fals if param search has to be redone
+
+    Returns: 
+        A dictionary containing optimal parameters for this training set. Also saves to file. 
+
+    """
+    if os.path.exists(filepath) and use_cached:
         with open(filepath, 'r') as file:
             return json.load(file)
     else:
@@ -18,11 +31,36 @@ def _optimal_parameters_rf(filepath:str, X:np.ndarray, y:np.ndarray) -> dict:
             json.dump(best_params, file)
         return best_params
 
-def get_untrained_rf_classifier(X, y):
-    optimal_params = _optimal_parameters_rf('src/models/rf_params.json', X, y)
+def get_untrained_rf_classifier(X:np.ndarray, y:np.ndarray, use_cached:bool=True) -> RandomForestClassifier:
+    """
+    Returns an untrained random forest classifier with optimal parameters found. If there are no
+    cached optimal parameters, these will be found using the supplied X and y. 
+    IMPORTANT: To prevent leakage, do *not* pass the entire dataset as that will influence parameter selection
+
+    Keyword arguments:
+        X: Training data
+        y: target labels
+        use_cached: True by default, set to false if param search has to be redone
+
+    Returns: 
+        An untrained random forest classifier with optimal parameters based on X/y sets. 
+    """
+    optimal_params = _optimal_parameters_rf('src/models/rf_params.json', X, y, use_cached)
     return RandomForestClassifier(**optimal_params)
 
-def get_trained_rf_classifier(X, y):
-    optimal_params = _optimal_parameters_rf('src/models/rf_params.json', X, y)
+def get_trained_rf_classifier(X:np.ndarray, y:np.ndarray, use_cached:bool=True) -> RandomForestClassifier:
+    """"
+    Returns a trained random forest classifier with optimal parameters found. If there are no
+    cached optimal parameters, these will be found using the supplied X and y. 
+
+    Keyword arguments:
+    X: data
+    y: target labels
+    use_cached: True by default, set to false if param search has to be redone
+
+    Returns: 
+    A random forest classifier trained on 
+    """
+    optimal_params = _optimal_parameters_rf('src/models/rf_params.json', X, y, use_cached)
     clf = RandomForestClassifier(**optimal_params)
     return clf.fit(X, y)